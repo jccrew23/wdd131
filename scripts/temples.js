3c1539f9af4dc86e590328ef7f5923e4a981a261
jccrew23 <cur24001@byui.edu>
2024-07-01 13:01:30 -0400

    design.wk3

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,3 +1,10 @@
 document.getElementById("currentyear").innerHTML = new Date().getFullYear();
 document.getElementById("lastModified").innerHTML = new Date(document.lastModified)
 
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click',() => {
+    navigation.classList.toggle('open');
+    hamButton.classList.toggle('open');
+})
