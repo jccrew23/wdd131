4e570360f21370e9e0019e07e1fa9e693f8d65ac
jccrew23 <cur24001@byui.edu>
2024-07-22 19:51:51 -0400

    index1

---
+++ project/scripts/home.js
@@ -0,0 +1,34 @@
+//button navigation
+const aboutButton = document.querySelector('#about-me');
+const priceButton = document.querySelector('#prices');
+const contactButton = document.querySelector('#contacts');
+
+aboutButton.addEventListener ('click', () => {
+    window.location.href = ''
+})
+
+priceButton.addEventListener ('click', () => {
+    window.location.href = ''
+})
+
+contactButton.addEventListener ('click', () => {
+    window.location.href = ''
+})
+
+//email prompt 
+let numVisits = Number(window.localStorage.getItem('numVisits-Is')) || 0;
+
+if (numVisits !== 0) {
+    window.prompt('Welcome Back! Want to join my Newsletter? ', 'john@gmail.com');
+}
+
+numVisits++;
+
+localStorage.setItem("numVisits-ls", numVisits);
+
+
+
+// footer copyright and modified date
+document.getElementById("currentyear").innerHTML = new Date().getFullYear();
+document.getElementById("lastModified").innerHTML = new Date(document.lastModified);
+
