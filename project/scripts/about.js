1ee65b5116d05316a7591aa06d8eb23c5f121485
jccrew23 <cur24001@byui.edu>
2024-07-23 14:04:23 -0400

    about1

---
+++ project/scripts/about.js
@@ -0,0 +1,44 @@
+const review = [
+    {
+        firstName: 'Liz',
+        stars: '⭐⭐⭐⭐⭐',
+        reviewpar: 'Our kids have learned so much here in just  year. Is has tremendously  helped with their piano skills and overall confidence with trying new things. Jalie is kind, patient and a very effective teacher. They have learned so much from her in a short amount time.'
+    },
+    {
+        firstName: 'Emily',
+        stars: '⭐⭐⭐⭐⭐',
+        reviewpar: 'We have been taking piano lessons at Creative Keys for almost one year now, both child and adult classes. They have the best teacher, and we feel so grateful about finding this gem!'
+    },
+    {
+        firstName: 'Sam',
+        stars: '⭐⭐⭐⭐⭐',
+        reviewpar: 'Jalie was the piano teacher for our two sons and was so wonderful. She is fun and gentle, and always made them feel so positively about the piano and their lessons. I would highly recommend her to anyone looking for a teacher who is passionate and great with kids.'
+    },
+    {
+        firstName: 'Paulie',
+        stars: '⭐⭐⭐⭐⭐',
+        reviewpar: 'Creative Keys is stellar at communicating with parents and students. My child takes lessons from Jalie and finds her helpful, kind and supportive as well as incredible knowledgeable about music theory. She has gained an immeasurable amount of confidence so far.'
+    }
+];
+
+function displayReview(sect) {
+    document.querySelector('#review').innerHTML = '';
+    sect.forEach(rev => {
+        const card = document.createElement('section');
+        const fname = document.createElement('h3');
+        const stars = document.createElement('p');
+        const reviewWord = document.createElement('p');
+
+        fname.innerHTML = rev.firstName;
+        stars.innerHTML = rev.stars;
+        reviewWord.innerHTML = rev.reviewpar;
+
+        card.appendChild(fname);
+        card.appendChild(stars);
+        card.appendChild(reviewWord);
+
+        document.querySelector('#review').appendChild(card);
+    })
+}
+
+displayReview(review);
