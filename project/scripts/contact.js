4199f24b829f26bfb259f831ca8d3d6dd6e99685
jccrew23 <cur24001@byui.edu>
2024-07-24 17:09:40 -0400

    contact1

---
+++ project/scripts/contact.js
@@ -0,0 +1,5 @@
+
+
+// footer copyright and modified date
+document.getElementById("currentyear").innerHTML = new Date().getFullYear();
+document.getElementById("lastModified").innerHTML = new Date(document.lastModified);
