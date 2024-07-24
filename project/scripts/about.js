385898018696f332b28e4241d29a12d844ed4863
jccrew23 <cur24001@byui.edu>
2024-07-24 17:17:40 -0400

    about footer

--- project/scripts/about.js
+++ project/scripts/about.js
@@ -42,3 +42,7 @@ function displayReview(sect) {
 }
 
 displayReview(review);
+
+// footer copyright and modified date
+document.getElementById("currentyear").innerHTML = new Date().getFullYear();
+document.getElementById("lastModified").innerHTML = new Date(document.lastModified);
